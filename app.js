9b7244eb6175c9a93c557ea7dde5f08b8220c639
Pavan Kavade <pkawade97@gmaill.com>
2018-12-27 10:54:43 +0530

    fixed database connectivity

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ const users = require('./routes/users');
 
 // Passport Config
 require('./config/passport')(passport);
-
-//DB config
-const db= require('./config/database');
+// DB Config
+const db = require('./config/database');
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -85,7 +84,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
