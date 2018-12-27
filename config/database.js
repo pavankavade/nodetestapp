ea65b85ffa020f93169e0b0164a360604bab3eef
Pavan Kavade <pkawade97@gmaill.com>
2018-12-27 11:00:20 +0530

    fixed database connectivity

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb://<pavan11>:<pavan1>@ds135394.mlab.com:35394/vidjot-prod1'}
+  module.exports = {mongoURI:'mongodb://<pavan11>:<pavan1>@ds135394.mlab.com:35394/vidjot-prod1'};
 } else {
-  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'};
 }
